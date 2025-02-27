848dc49ffb8c0294ea35b56f29286004882eb6b7
defendimattia <defendi.mattia92@gmail.com>
2025-02-27 15:13:48 +0100

    add: solution dadi

---
+++ dadi/script.js
@@ -0,0 +1,12 @@
+// dichiarare 2 variabili e assegnargli un numero randomico da 1 a 6 ad ognuno
+const userNum = Math.floor(Math.random() * (7 - 1) + 1);
+const computerNum = Math.floor(Math.random() * (7 - 1) + 1);
+
+// dichiarare una condizione che printi il numero più alto o se entrambi uguali un messaggio di pareggio
+if (userNum > computerNum) console.log("Hai vinto!");
+else if (userNum < computerNum) console.log("Hai perso!");
+else console.log("Pareggio!");
+   
+console.log(
+    `Il tuo numero: ${userNum}
+Il numero dell'avversario: ${computerNum}`);
