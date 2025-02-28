d36166bb8013d85d0382bb14493bbe6dbd587c7d
defendimattia <defendi.mattia92@gmail.com>
2025-02-28 10:51:53 +0100

    fix: math code

--- dadi/dadi-bonus/script.js
+++ dadi/dadi-bonus/script.js
@@ -2,7 +2,7 @@
 // in una variabile al posto di assegnare numero randomico, assegnare un prompt di ind con massimo da 1 a 6
 
 let userNum = parseFloat(prompt("Inserisci un numero da 1 a 6!"));
-const computerNum = Math.floor(Math.random() * (7 - 1) + 1);
+const computerNum = Math.floor(Math.random() * 6) + 1;
 
 
 if (!(userNum >= 1 && userNum <= 6)) {
--- dadi/script.js
+++ dadi/script.js
@@ -1,6 +1,6 @@
 // dichiarare 2 variabili e assegnargli un numero randomico da 1 a 6 ad ognuno
-const userNum = Math.floor(Math.random() * (7 - 1) + 1);
-const computerNum = Math.floor(Math.random() * (7 - 1) + 1);
+const userNum = Math.floor(Math.random() * 6) + 1;
+const computerNum = Math.floor(Math.random() * 6) + 1;
 
 // dichiarare una condizione che printi il numero più alto o se entrambi uguali un messaggio di pareggio
 if (userNum > computerNum) console.log("Hai vinto!");
